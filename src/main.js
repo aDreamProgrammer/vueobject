0dd34dbba24391d7d0b2d9e904fd43440dcd3e27
相宁 <1735622558@qq.com>
2020-02-24 19:02:52 +0800

    底部完成

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import Mint from 'mint-ui';
 import 'mint-ui/lib/style.css'
+import './assets/mui/css/mui.css'
+
 Vue.use(Mint)
 Vue.config.productionTip = false
 
